3b914a05f044fd143594824bfa351e986ade3a93
Peter Standing <44238507+PeterStanding@users.noreply.github.com>
2026-02-28 16:26:25 +0000

    Added Text to the Picture

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import pandas as pd
+import pickle
 
 # Reading the DataSets for both One and Two hands
 oneHand = pd.read_csv("one_hand_dataset.csv")
 twoHand = pd.read_csv("two_hand_dataset.csv")
 
-print("One Handed DataSet:", oneHand.shape)
-print(oneHand.head())
 
 base_options = python.BaseOptions(model_asset_path = 'hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options = base_options,
@@ -21,7 +20,7 @@ options = vision.HandLandmarkerOptions(base_options = base_options,
 detector = vision.HandLandmarker.create_from_options(options)
 
 cap = cv2.VideoCapture(0)
-'''
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -34,7 +33,7 @@ while cap.isOpened():
     results = detector.detect(mp_image)
 
     r = g = b = 0
-
+    # Tracks the Hand landmarks Positions
     if results.hand_landmarks:
         for hand_landmarks in results.hand_landmarks:
             # Different HandLandmarks
@@ -159,12 +158,13 @@ while cap.isOpened():
             cv2.line(frame,(ppip_x,ppip_y), (pdip_x,pdip_y), (102,255,102),2)
             cv2.line(frame,(pdip_x,pdip_y), (ptip_x,ptip_y), (102,255,102),2)
 
-    cv2.imshow("Sign Language Interpreter", frame)      
-
+    #cv2.putText(image, text, org, font, fontScale, color, thickness=1, lineType=cv2.LINE_8, bottomLeftOrigin=False)      
+    cv2.putText(frame, "Testing", (30,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+    cv2.imshow("Sign Language Interpreter", frame)
+    
     #quitting
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-'''
